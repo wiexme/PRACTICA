22420edcc39948dbd437b33020cfed2fe5358c4a
GESSEÑY <GESSEY@192.168.93.63>
2015-03-23 15:43:22 -0500

    Prueba2

--- practicando/src/practicando/Practicando.java
+++ practicando/src/practicando/Practicando.java
@@ -16,7 +16,9 @@ public class Practicando {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String nombre;
+        String apellido;
+        
     }
     
 }
