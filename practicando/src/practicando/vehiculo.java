16079d3d77128f9eda6439605d3ddbe3ccd740d4
GESSEÑY <GESSEY@192.168.93.63>
2015-03-23 17:24:42 -0500

    Prueba2. Llenando sus atributos a la clase vehiculo

--- practicando/src/practicando/vehiculo.java
+++ practicando/src/practicando/vehiculo.java
@@ -26,5 +26,14 @@ public class vehiculo {
     }
     
     public void apagar(){
+        System.out.println("El vehiculo se apago!!");
     }    
+     public void Encender(String Marca){
+       System.out.println("El vehiculo"+Marca+" se encendio!!");
+    
+    }
+    public void Encender(String Marca, int NumeroPuertas){
+       System.out.println("El vehiculo"+Marca+" con "+NumeroPuertas+" se encendio!!");
+    
+    }
 }
