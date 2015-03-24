a46117b8d13f9413c1b6ea031c0115f77db09f67
WIEXME <WIEXME@192.168.1.44>
2015-03-23 20:08:35 -0500

    bueo auqui jugando otravez

--- practicando/src/practicando/moto.java
+++ practicando/src/practicando/moto.java
@@ -4,14 +4,20 @@ package practicando;
 
 
 public class moto extends vehiculo{
+    String modelo;
+    int numrueda;
     String marca;
+    
 
     public moto(String marca) {
         this.marca = marca;
     }
     
     public void comenzar() {
-        this.Encender(marca);
+            this.Encender(marca);
+       
+        
+        
         
         
     }
