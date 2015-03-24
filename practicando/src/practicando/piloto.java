3d726e94162da3418c9a6294652437fa1d8f6d86
GESSEÑY <GESSEY@192.168.93.63>
2015-03-23 20:16:04 -0500

    Creando la clase piloto,  llenando con sus atributos y realizando la presentacion de la clase.

---
+++ practicando/src/practicando/piloto.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package practicando;
+
+/**
+ *
+ * @author GESSEÑY
+ */
+public class piloto {
+    
+    int edad= 20;
+    String nombre="Geseñi Trigozo";
+    String rango="Capitan";
+    
+    public void Presentarse(){
+        System.out.print("Hola mi nombre es: "+this.nombre+".  Mi edad es: "+this.edad+" Y soy el rango: "+this.rango);
+    }
+    
+}
